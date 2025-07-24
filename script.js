1e899c21fd3028e8bdbeeb3cae3e8e396f44e64e
FrontEnddeveloping77 <frontenddeveloping1@gmail.com>
2025-07-24 09:29:45 +0500

    kalkulatir

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function hisobla(amal) {
   const son1 = +document.getElementById("son1").value;
   const son2 = +document.getElementById("son2").value;
-  const output = document.getElementById("natija");
+  const p = document.getElementById("natija");
 
   if (
     !document.getElementById("son1").value ||
     !document.getElementById("son2").value
   ) {
-    output.textContent = "Raqamlar kiritiladigan joyni to'ldiring!";
-    output.style.color = "red";
-    output.style.fontSize = "26px";
-    output.style.marginTop = "15px";
-    output.style.fontWeight = "700";
+    p.textContent = "Raqamlar kiritiladigan joyni to'ldiring!";
+    p.style.color = "red";
+    p.style.fontSize = "26px";
+    p.style.marginTop = "15px";
+    p.style.fontWeight = "700";
     return;
   }
 
@@ -28,19 +28,13 @@ function hisobla(amal) {
       natija = son1 * son2;
       break;
     case "/":
-      if (son2 !== 0) {
-        natija = son1 / son2;
-      } else {
-        output.textContent = "0 ga bo‘lish mumkin emas!";
-        output.style.color = "red";
-        return;
-      }
+      natija = son1 / son2;
       break;
   }
 
-  output.textContent = "Natija: " + natija;
-  output.style.color = "#00ff88";
-  output.style.fontSize = "26px";
-  output.style.marginTop = "15px";
-  output.style.fontWeight = "700";
+  p.textContent = "Natija: " + natija;
+  p.style.color = "#00ff88";
+  p.style.fontSize = "26px";
+  p.style.marginTop = "15px";
+  p.style.fontWeight = "700";
 }
